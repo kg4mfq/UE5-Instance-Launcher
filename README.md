c31913b142eeb8f157a9c92195c493111385e4d2
XeroSpace <kg4mfq@gmail.com>
2023-04-20 07:13:49 -0500

    Change for MariaDB 10 Character Set

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # v0dka-Instance-Launcher
 this is a instance launcher for unreal engine dedicated servers
 
-Modified to use MariaDB instead of MySql
+Modified to use MariaDB instead of MySql.
+This is targeting install on Debian 11 and should work on derivatives such as Ubuntu
--- database/current.sql
+++ database/current.sql
@@ -27,7 +27,7 @@ CREATE TABLE `dedicated_servers` (
   `id` int NOT NULL AUTO_INCREMENT,
   `server_ip` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_520_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
